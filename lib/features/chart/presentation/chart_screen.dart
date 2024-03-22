8eb47a3b8d6c7eb748287420b0d8cc1799a59e35
Eric <winricyu@gmail.com>
2024-03-22 10:40:58 +0800

    - 調整供餐統計排版

--- lib/features/chart/presentation/chart_screen.dart
+++ lib/features/chart/presentation/chart_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exam_jubo/features/meal/provider/breakfast_provider.dart';
+import 'package:flutter_exam_jubo/features/meal/provider/dinner_provider.dart';
+import 'package:flutter_exam_jubo/features/meal/provider/lunch_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends ConsumerWidget {
   const ChartScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ChartScreen'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final breakfastCounts = ref.watch(breakfastCounterProvider);
+    final lunchCounts = ref.watch(lunchCounterProvider);
+    final dinnerCounts = ref.watch(dinnerCounterProvider);
+
+    return Scaffold(
+      body: SizedBox.expand(
+        child: ColoredBox(
+          color: Colors.orange.shade50,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '早餐：$breakfastCounts',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Text(
+                '午餐：$lunchCounts',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Text(
+                '晚餐：$dinnerCounts',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
